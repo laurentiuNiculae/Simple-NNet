d7b1a289b02cdb7cbcd908fb7eea9e3c84469575
Laurentiu Niculae <niculae.laurentiu1@gmail.com>
2023-05-22 08:44:53 +0300

    W

--- gym.py
+++ gym.py
@@ -1,4 +1,17 @@
 import pygame
 
+pygame.init()
+
 if __name__ == "__main__":
-    
+    pygame.init()
+    screen = pygame.display.set_mode((800,400))
+    pygame.display.set_caption('Runner')
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+		
+        
